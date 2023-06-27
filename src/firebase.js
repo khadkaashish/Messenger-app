19991a298b3c42523c4a28219af0e50b102cf987
Ashish Khadka <ashishkhadka03@gmail.com>
2023-06-27 11:31:56 +0545

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBYQoL7LpFN51UsSNqhvurfqa0eyWlAZGo",
-  authDomain: "marshmallow02.firebaseapp.com",
-  projectId: "marshmallow02",
-  storageBucket: "marshmallow02.appspot.com",
-  messagingSenderId: "730855413200",
-  appId: "1:730855413200:web:a5406622c49496dd4dd5d5",
+  apiKey: "AIzo",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "73",
+  appId: "1:7308",
 };
 
 // Initialize Firebase
